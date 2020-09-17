1255d3f79cc660168fba3e1c6db692a65e47ae19
BuildTools <unconfigured@null.spigotmc.org>
2020-09-17 22:25:15 +0300

    Some improvements

--- TestWebApp/Program.cs
+++ TestWebApp/Program.cs
@@ -18,9 +18,6 @@ namespace TestWebApp
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
--- TestWebApp/TestModels/SimpleTestClass.cs
+++ TestWebApp/TestModels/SimpleTestClass.cs
@@ -1,7 +1,20 @@
-﻿namespace TestWebApp.TestModels
+﻿using System;
+
+namespace TestWebApp.TestModels
 {
     public class SimpleTestClass
     {
-        
+        public int Summ(params int[] values)
+        {
+            int sum = 0;
+            foreach (int value in values)
+            {
+                if (value <= 0)
+                    continue;
+                sum += value;
+            }
+
+            return sum;
+        }
     }
 }
