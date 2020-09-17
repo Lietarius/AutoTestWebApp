64324cdebe961f500408d0fa9098b70d366b79b4
BuildTools <unconfigured@null.spigotmc.org>
2020-09-17 22:24:03 +0300

    Add simple component

---
+++ TestWebApp/ClientApp/src/components/SimpleComponent.tsx
@@ -0,0 +1,35 @@
+﻿import React, { Component } from 'react';
+
+export default class SimpleComponent extends React.Component<any, { count: number }> {
+    constructor(props:any) {
+        super(props);
+        
+        this.state = {
+            count: 0,
+        }
+        
+    }
+    
+    render () {
+        return (<div>
+            <h3>This is simple react component</h3>
+            <div>
+                <span className={"counterState"}>Counter state is {this.state.count}</span>
+                <button onClick={()=>{this.handleIncrement()}}>+1</button>
+                <button onClick={()=>{this.handleDecrement()}}>-1</button>
+            </div>
+        </div>);
+    }
+
+    handleIncrement() {
+        this.setState({
+            count: this.state.count +1
+        })
+    }
+
+    handleDecrement() {
+        this.setState({
+            count: this.state.count -1
+        })
+    }
+}
